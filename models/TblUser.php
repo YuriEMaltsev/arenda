3839aef40421dfec5a2932e258051fe895bf0960
maltsev_ue <ye.maltsev@yandex.ru>
2016-08-17 18:47:18 +0700

    32311

--- models/TblUser.php
+++ models/TblUser.php
@@ -41,8 +41,43 @@ class TblUser extends \yii\db\ActiveRecord
             [['phone'], 'string', 'max' => 10],
             [['pasp_ser', 'drive_lic_ser'], 'string', 'max' => 4],
             [['pasp_num', 'drive_lic_num'], 'string', 'max' => 6],
+
+            [['e-mail', 'passw'], 'required', 'message' => 'error'],
+//            [ 'email', 'allowEmpty' => false, 'checkMX' => true, 'message' => 'error'],
+            ['passw', 'authenticate']
         ];
     }
+
+    public function authenticate($attribute,$params)
+    {
+        echo "authenti /n";
+        $this->_identity = Account::model()->checkLogin();
+        if(!$this->_identity)
+            $this->addError('password', 'Пароль или имя пользователя неправильный');
+    }
+/*
+    public function primaryKey()
+    {
+        return 'id';
+    }
+*/
+
+//    public function checkLogin($email, md5($password))
+    public function checkLogin()
+    {
+        //echo "check LOGIN e-mail=$this->e_mail and passw=$this->passw /n";
+        $user = $this-> findAll("e-mail=$this->e_mail and passw=$this->passw");
+        //findByAttributes(array('e-mail' => $this->e_mail  , 'passw' => $this->passw));
+
+        if($user===null)
+        {
+          return false;
+        }
+
+        // установить ID user
+        return true;
+    }
+
 
     /**
      * @inheritdoc
@@ -54,13 +89,13 @@ class TblUser extends \yii\db\ActiveRecord
             'id_type_client' => 'Id Type Client',
             'e_mail' => 'E Mail',
             'user_name' => 'User Name',
-            'passw' => 'Passw',
-            'name_fio' => 'Name Fio',
-            'phone' => 'Phone',
-            'pasp_ser' => 'Pasp Ser',
-            'pasp_num' => 'Pasp Num',
-            'drive_lic_ser' => 'Drive Lic Ser',
-            'drive_lic_num' => 'Drive Lic Num',
+            'Пароль' => 'Passw',
+            'ФИО' => 'Name Fio',
+            'Телефон' => 'Phone',
+            'Паспорт серия' => 'Pasp Ser',
+            'Паспорт номер' => 'Pasp Num',
+            'Водительское удостоверение серия' => 'Drive Lic Ser',
+            'Водительское удостоверение номер' => 'Drive Lic Num',
         ];
     }
 }
